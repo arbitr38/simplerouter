9d55cbae8a6031bcbfd2e361c4f0ff8ee10dc049
arbitr38 <arbitr38@gmail.com>
2018-01-01 11:00:33 +0800

    2n commit

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,27 +1,37 @@
 package handlers
 
 import (
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestHome(t *testing.T) {
-	w := httptest.NewRecorder()
-	home(w, nil)
+func TestRouter(t *testing.T) {
+	r := Router()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
 
-	resp := w.Result()
-	if have, want := resp.StatusCode, http.StatusOK; have != want {
-		t.Errorf("Status code is wrong. Have: %d, want: %d.", have, want)
-	}
-
-	greeting, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
+	res, err := http.Get(ts.URL + "/home")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if have, want := string(greeting), "Hello! Your request was processed."; have != want {
-		t.Errorf("The greeting is wrong. Have: %s, want: %s.", have, want)
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code for /home is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusOK)
+	}
+
+	res, err = http.Post(ts.URL+"/home", "text/plain", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status code for /home is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusMethodNotAllowed)
+	}
+
+	res, err = http.Get(ts.URL + "/not-exists")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusNotFound {
+		t.Errorf("Status code for /home is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusNotFound)
 	}
 }
