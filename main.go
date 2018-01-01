a8b37e065b2053413ac396d2d345ff9357d79425
arbitr38 <arbitr38@gmail.com>
2018-01-01 09:52:31 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"handlers"
+)
+
+func main() {
+	log.Print("Startin the service...")
+	router := handlers.Router()
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
